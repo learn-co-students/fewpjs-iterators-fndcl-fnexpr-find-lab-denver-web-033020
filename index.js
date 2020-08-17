1e5291a1cd290a6322e91967c084dd974b734ca9
kathryn0908 <kathrynlael@gmail.com>
2020-08-17 17:27:55 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+var record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+function superbowlWin(record){
+  let sb = record.find(r => r.result === "W")
+    return sb ? sb.year : undefined
+}
+
+console.log(superbowlWin(record))
